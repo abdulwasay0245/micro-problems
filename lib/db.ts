8335ce7cc53b6481e6bfee14352db65f37ad2f76
Abdul Wasay <abdulwasay0245@gmail.com>
2026-03-14 00:57:33 +0500

    fix(db): temporarily hardcode database URL for debugging vercel edge environment variables

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,6 @@ import { neon } from '@neondatabase/serverless';
 
 // The Neon driver allows you to execute raw SQL directly.
 // We use a fallback empty string for Next.js build-time prerendering
-const databaseUrl = process.env.DATABASE_URL || '';
+const databaseUrl =  'postgresql://neondb_owner:npg_7rv3cjVJdklZ@ep-broad-forest-a1k9uvnv-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 
 export const sql = neon(databaseUrl);
