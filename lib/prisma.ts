e64e8eb0318350292b83032c6de0c21174b488c4
Abdul Wasay <abdulwasay0245@gmail.com>
2026-03-14 00:19:33 +0500

    minor

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,10 @@
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../app/generated/prisma';
-
-const connectionString = `${process.env.DATABASE_URL}&connect_timeout=30`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
